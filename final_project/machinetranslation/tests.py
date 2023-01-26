3819c242eee42edd6822dedf1badbf95dfa03cf8
antoinepjr <antoinepjr@gmail.com>
2023-01-25 21:39:39 -0500

    final commit

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,12 +4,12 @@ from translator import french_to_english, english_to_french
 
 class TestEtoF(unittest.TestCase):
     def test1(self):
-        self.assertIsNot(english_to_french(''), 'Bonjour')
+        self.assertIsNot(english_to_french('Hello'), None)
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
         
 class TestFtoE(unittest.TestCase):
     def test1(self):
-        self.assertIsNot(french_to_english(''), 'Hello')
+        self.assertIsNot(french_to_english('Bonjour'), None)
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -16,24 +16,20 @@ url = os.environ['url']
 authenticator = IAMAuthenticator(apikey)
 
 try:
-    language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+    language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)    
 except ApiException as ex:
     print("Method failed with status code " + str(ex.code) + ": " + ex.message)
 
 
 language_translator.set_service_url(url)
 
-language_translator.set_disable_ssl_verification(True)
-
-
-
 
 def english_to_french(english_text):
     """Change to french"""
-    french_text = language_translator.translate(text = english_text, model_id='en-fr').get_result
-    return french_text
+    french_text = language_translator.translate(text = english_text, model_id='en-fr').get_result()
+    return french_text['translations'][0]['translation']
 
 def french_to_english(french_text):
     """Change to english"""
-    english_text = language_translator.translate(text = french_text, model_id='fr-en').get_result
-    return english_text
+    english_text = language_translator.translate(text = french_text, model_id='fr-en').get_result()
+    return english_text['translations'][0]['translation']
